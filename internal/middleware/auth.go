a942f08b55e8a747a1c10f3abf9e3168831ab099
Siska Stevani <44636088+siskastev@users.noreply.github.com>
2023-08-30 00:16:32 +0700

    feat(auth):
    add auth middleware

---
+++ internal/middleware/auth.go
@@ -0,0 +1,51 @@
+package middleware
+
+import (
+	"siska-rgb-golang-test/internal/helpers/jwt"
+	"siska-rgb-golang-test/internal/models"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type AuthMiddleware struct {
+	SecretKey string
+}
+
+func NewAuthMiddleware(secretKey string) *AuthMiddleware {
+	return &AuthMiddleware{SecretKey: secretKey}
+}
+
+func (a *AuthMiddleware) AuthRequired() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		authHeader := c.Get("Authorization")
+		if authHeader == "" {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Unauthorized"})
+		}
+
+		authToken := strings.Split(authHeader, " ")[1]
+		user, err := jwt.VerifyAndExtractUserFromJWT(authToken)
+		if err != nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Unauthorized"})
+		}
+
+		c.Locals("user", user)
+
+		return c.Next()
+	}
+}
+
+func (a *AuthMiddleware) IsAdmin() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		user, found := c.Locals("user").(*models.UserResponse)
+		if !found {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Unauthorized"})
+		}
+
+		if user.Role != models.ADMIN_ROLE {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"message": "Unauthorized Role Access"})
+		}
+
+		return c.Next()
+	}
+}
