597d2cac231df2c6f639596a9cc9904ea59b723f
Siska Stevani <44636088+siskastev@users.noreply.github.com>
2023-08-30 00:12:58 +0700

    feat(users):
    add seeder insert user admin

--- internal/database/migrations/20230829073946_insert_user_admin.up.sql
+++ internal/database/migrations/20230829073946_insert_user_admin.up.sql
@@ -1,2 +1,2 @@
-INSERT INTO users(email,name,password,role)
-VALUES ('admin@rgb.id','admin','$2a$12$UTzg8w1/4Syi/037e8sv8.77kPN0TURwSlw01xkWEM4PUAFRRIRKG','admin');
+INSERT INTO users(email,name,password,role, created_at, updated_at)
+VALUES ('admin@rgb.id','admin','$2a$12$UTzg8w1/4Syi/037e8sv8.77kPN0TURwSlw01xkWEM4PUAFRRIRKG','admin','now()','now()');
